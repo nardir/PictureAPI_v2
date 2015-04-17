e9c9cd9cc9f44c8be82fe338ac6d368faf436d52
nardir <nardir@axerrio.com>
2015-04-17 16:01:46 +0200

    Stock tables

---
+++ AxerrioOnline/dbo/Tables/Stock.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Stock] (
+    [StockKey]           INT           IDENTITY (1, 1) NOT NULL,
+    [StockTypeId]        INT           NOT NULL,
+    [LogisticIdentifier] NVARCHAR (50) NULL,
+    [Created]            DATETIME      CONSTRAINT [DF_Stock_Created] DEFAULT (getdate()) NOT NULL,
+    [Modified]           DATETIME      CONSTRAINT [DF_Stock_Modified] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_Stock] PRIMARY KEY CLUSTERED ([StockKey] ASC),
+    CONSTRAINT [FK_Stock_StockType] FOREIGN KEY ([StockTypeId]) REFERENCES [dbo].[StockType] ([StockTypeId])
+);
+GO
+CREATE NONCLUSTERED INDEX [IX_FK_Stock_StockType]
+    ON [dbo].[Stock]([StockTypeId] ASC);
+GO
---
+++ AxerrioOnline/dbo/Tables/StockType.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[StockType] (
+    [StockTypeId]          INT            NOT NULL,
+    [Name]                 NVARCHAR (100) NOT NULL,
+    [Description]          NVARCHAR (255) NULL,
+    [AmountValuePrecision] INT            CONSTRAINT [DF_StockType_AmountValuePrecision] DEFAULT ((0)) NOT NULL,
+    [Created]              DATETIME       CONSTRAINT [DF_StockType_Created] DEFAULT (getdate()) NOT NULL,
+    [Modified]             DATETIME       CONSTRAINT [DF_StockType_Modified] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_StockType] PRIMARY KEY CLUSTERED ([StockTypeId] ASC)
+);
