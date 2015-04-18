6834d42526d53c5cf984f195e46cd746122269e9
Nardi <nardir@axerrio.com>
2015-04-18 13:45:27 +0200

    Azure database

---
+++ AxerrioOnline/Security/Login_nrtest.sql
@@ -0,0 +1,1 @@
+﻿CREATE LOGIN [nrtest] WITH PASSWORD = 'Vexcherk1'
---
+++ AxerrioOnline/Security/User_nrtest.sql
@@ -0,0 +1,9 @@
+﻿CREATE USER nrtest
+	FOR LOGIN nrtest
+	WITH DEFAULT_SCHEMA = dbo
+GO
+-- Add user to the database owner role
+EXEC sp_addrolemember N'db_datareader', N'nrtest'
+GO
+EXEC sp_addrolemember N'db_datawriter', N'nrtest'
+GO
